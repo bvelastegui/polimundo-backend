e82546e1009b5446acf70c242b0412c50ecdf530
Bryan Velastegui <bryanslvr@gmail.com>
2022-03-23 19:23:01 -0500

    Compañías Utilizando el recurso http se crea el endpoint que lista todas las compañías y el endpoint que muestra la compañía por 'id'

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\CompanyResource;
+use App\Models\Company;
+use Illuminate\Contracts\Support\Responsable;
+
+class CompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Support\Responsable
+     */
+    public function index(): Responsable
+    {
+        $collection = Company::all();
+
+        return CompanyResource::collection($collection);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Company $company
+     * @return \Illuminate\Contracts\Support\Responsable
+     */
+    public function show(Company $company): Responsable
+    {
+        return new CompanyResource($company);
+    }
+}
