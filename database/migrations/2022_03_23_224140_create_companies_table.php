ec8a49ad4db164fd06090a955c4ecf3edc845f15
Bryan Velastegui <bryanslvr@gmail.com>
2022-03-23 19:09:35 -0500

    [WIP] Compañías Se crea la tabla companies

---
+++ database/migrations/2022_03_23_224140_create_companies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('support_email');
+            $table->string('support_phone')->nullable();
+            $table->date('foundation_date');
+            $table->float('price', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+};
