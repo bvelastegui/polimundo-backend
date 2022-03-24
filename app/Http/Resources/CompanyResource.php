7d526c13169631ed1ec800afdde77c2f32a4ffe5
Bryan Velastegui <bryanslvr@gmail.com>
2022-03-23 19:18:50 -0500

    [WIP] Compañías Se crea el recurso http para renderizar los datos del modelo

---
+++ app/Http/Resources/CompanyResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CompanyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'address' => explode("\n", $this->address),
+            'support_email' => $this->support_email,
+            'support_phone' => $this->support_phone,
+            'foundation' => [
+                'date' => $this->foundation_date->format('Y-m-d'),
+                'age' => $this->foundation_date->age
+            ],
+            'price' => $this->price
+        ];
+    }
+}
